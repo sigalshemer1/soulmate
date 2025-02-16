5212015dcc925fd85aa35df1bb92691d40d9174e
sigalshemer <sigal.shemer68@gmail.com>
2025-02-16 10:39:17 +0000

    first

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,35 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { StyleSheet,View ,Image,Text } from 'react-native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
+import HomeScreen from './Home';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import logo from '../assets/images/logo.png';
+
+export const HeaderLogo = () => (
+  <Image
+    source={logo}
+    style={{ width: 200, height: 60, resizeMode: 'contain' }}
+  />
+);
+
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
+const RootLayout = () => {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
+
+  const Tab = createBottomTabNavigator();
 
   useEffect(() => {
     if (loaded) {
@@ -29,11 +43,49 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Tab.Navigator 
+      screenOptions={{
+        tabBarStyle: styles.tabBar,
+        tabBarActiveTintColor: '#000',
+        tabBarInactiveTintColor: '#898889',
+        headerStyle: styles.header,
+        headerTintColor: '#898889',
+        headerTitle: () => <HeaderLogo />,
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="home-outline" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+    <StatusBar style="auto" />
+  </ThemeProvider>
   );
 }
+
+
+const styles = StyleSheet.create({
+  tabBar: {
+    backgroundColor: '#59aec2',
+    borderTopWidth: 0,
+  },
+  header: {
+    backgroundColor: '#59aec2',
+    elevation: 0,
+  },
+  top:{
+    backgroundColor: '#59aec2',
+  },
+  headerTitle: {
+    fontWeight: 'bold',
+    fontSize: 22,
+  },
+});
+
+export default RootLayout;
